c66bb75206f6970cab39348a0e33e08168bc46da
Olena Posternak <alena.kavyrshyna@gmail.com>
2022-10-19 15:15:49 +0300

    phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,85 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import { Component } from 'react';
+import { AppStyled } from './App.module';
+import { ContactForm } from './ContactForm/ContactForm ';
+import { ContactsList } from './ContactsList/ContactsList';
+import { Filter } from './Filter/Filter';
+// import { AlertInfo } from './Alerts/Alert';
+
+import { PropTypes } from 'prop-types';
+
+const shortid = require('shortid');
+const contactId = shortid.generate();
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onSubmitHendler = data => {
+    const contact = {
+      id: contactId,
+      name: data.name,
+      number: data.number,
+    };
+
+    const contactName = [];
+
+    for (const contact of this.state.contacts) {
+      contactName.push(contact.name);
+    }
+
+    if (contactName.includes(contact.name)) {
+      alert(`${contact.name} is already in contacts list`);
+      return;
+    }
+
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  filterName = event => {
+    console.log(event.currentTarget.value);
+    this.setState({ filter: event.currentTarget.value });
+  };
+
+  delete = contactId => {
+    console.log(contactId);
+
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contactId !== contact.id),
+    }));
+  };
+
+  render() {
+    const filterNormilized = this.state.filter.toLowerCase().trim();
+    const visibleContacts = this.state.contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filterNormilized)
+    );
+
+    return (
+      <AppStyled>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmitForm={this.onSubmitHendler} />
+        <Filter value={this.state.filter} onChengeFilter={this.filterName} />
+        <ContactsList contacts={visibleContacts} deleteContact={this.delete} />
+        {/* <AlertInfo /> */}
+      </AppStyled>
+    );
+  }
+}
+
+App.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.string,
+      name: PropTypes.string,
+      number: PropTypes.string,
+    })
+  ),
+  filter: PropTypes.string,
+  onSubmitHendler: PropTypes.func,
+  delete: PropTypes.func,
+  filterName: PropTypes.func,
 };
---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,17 @@
+import { PropTypes } from 'prop-types';
+import {FilterSection, Label, Input} from './Filter.modul'
+
+export const Filter = ({ value, onChengeFilter }) => {
+  console.log(onChengeFilter);
+  return (
+    <FilterSection>
+      <Label>Filter</Label>
+      <Input type="text" value={value} onChange={onChengeFilter} />
+    </FilterSection>
+  );
+};
+
+Filter.prototype = {
+  value: PropTypes.string,
+  onChengeFilter: PropTypes.func,
+};
